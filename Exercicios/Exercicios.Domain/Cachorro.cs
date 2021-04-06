a21aa8927213eeee19786109b0dc1e2e94704750
gluisjulian <gluisjulian@gmail.com>
2021-04-05 21:15:29 -0300

    Exercicio1

---
+++ Exercicios/Exercicios.Domain/Cachorro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercicios.Domain
+{
+    public class Cachorro
+    {
+        public string Latir()
+        {
+            return "Au Au";
+        }
+
+        public string QuantoDevoComer(int pesoKg)
+        {
+            //MANEIRA 1 DE RESOLVER
+            //double qntdRacaoGramas = pesoKg * 50;
+            //string msg = "Como tenho " + pesoKg + "kg, devo comer " + qntdRacaoGramas + "g por dia"; //concatenação "simples"
+            //string msg = $"Como tenho {pesoKg}kg, devo comer {qntdRacaoGramas}g por dia"; //concatenação "interpolação"
+            //return msg;
+
+            //MANEIRA 2 DE RESOLVER
+           return $"Como tenho {pesoKg}kg, devo comer {pesoKg * 50}g por dia";
+        }
+    }
+}
---
+++ Exercicios/HelloWorldTest/CachorroTest.cs
@@ -0,0 +1,63 @@
+﻿using Exercicios.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace HelloWorldTest
+{
+    [TestClass]
+    public class CachorroTest
+    {
+        [TestMethod]
+        public void Cachorro_Latir_Test()
+        {
+            Cachorro leia = new Cachorro();
+            string latido = leia.Latir();
+
+            Console.WriteLine(latido);
+
+            Assert.AreEqual("Au Au", latido);
+        }
+
+
+        //Cachorro Leia
+        [TestMethod]
+        public void Leia_QuantoDevoComer_Test()
+        {
+            Cachorro leia = new Cachorro();
+            string quantoDevoComer = leia.QuantoDevoComer(1);
+
+            Console.WriteLine(quantoDevoComer);
+
+            Assert.AreEqual("Como tenho 1kg, devo comer 50g por dia", quantoDevoComer);
+
+        }
+
+
+        //Cachorro Tequila
+        [TestMethod]
+        public void Tequila_QuantoDevoComer_Test()
+        {
+            Cachorro tequila = new Cachorro();
+            string quantoDevoComer = tequila.QuantoDevoComer(30);
+
+            Console.WriteLine(quantoDevoComer);
+
+            Assert.AreEqual("Como tenho 30kg, devo comer 1500g por dia", quantoDevoComer);
+
+        }
+
+
+        //Cachorro Yuri
+        [TestMethod]
+        public void Yuri_QuantoDevoComer_Test()
+        {
+            Cachorro yuri = new Cachorro();
+            string quantoDevoComer = yuri.QuantoDevoComer(15);
+
+            Console.WriteLine(quantoDevoComer);
+
+            Assert.AreEqual("Como tenho 15kg, devo comer 750g por dia", quantoDevoComer);
+
+        }
+    }
+}
